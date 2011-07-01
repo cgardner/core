83a50ba55bcf78ac60b829a7c1dba56c6fcfd95f
Jason Socha <jason@seabourneconsulting.com>
2011-07-01 14:10:43 -0500

    adding core auth component and stubs for google, facebook, and twitter

---
+++ components/Authentication/CumulaAuth.interface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Cumula
+ *
+ * Cumula — framework for the cloud.
+ *
+ * @package    Cumula
+ * @version    0.1.0
+ * @author     Seabourne Consulting
+ * @license    MIT License
+ * @copyright  2011 Seabourne Consulting
+ * @link       http://cumula.org
+ */
+
+/**
+ * CumulaAuth
+ *
+ * Interface for authentication classes
+ *
+ * @package		Cumula
+ * @subpackage	Core
+ * @author     Seabourne Consulting
+ */
+interface CumulaAuth {
+  
+  /**
+   * The authenticate function of an auth class must take a set of parameters, 
+   * usually credentials, and return a response array.
+   * @param $params array of auth params
+   * @return array response from auth service
+   */
+	public function authenticate($params);
+  
+}
---
+++ components/Authentication/libraries/twitter.php
@@ -0,0 +1,31 @@
+<?php
+
+class twitterAuthentication extends Authentication implements CumulaAuth
+{
+  protected $success = FALSE;
+  
+  /**
+   * @var array - Populated with the response
+   */
+  protected $response = array();
+
+  /**
+   * @param $params array of auth params
+   * @return array response from auth service
+   */
+	public function authenticate($params)
+  {
+    $this->response = array('not implemented yet');
+    return $this->response;
+  }
+  
+  /**
+   * Getter for $success
+   */
+  public function success()
+  {
+    return $this->success;
+  }
+
+  
+}
