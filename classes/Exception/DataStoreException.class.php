6c8ee5704bb72ad5d00b2ac1996c7662d12ba304
Craig Gardner <craig@seabourneinc.com>
2011-08-10 17:57:47 -0700

    added the DataStoreException class

---
+++ classes/Exception/DataStoreException.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * DataStoreException
+ * @package Cumula
+ * @author Craig Gardner <craig@seabourneconsulting.com>
+ **/
+class DataStoreException extends Exception 
+{
+	
+} // end class DataStoreException extends Exception
