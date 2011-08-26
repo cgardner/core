a47a3a62241693d06cba65cf303a86e99e32f691
Craig Gardner <craig@seabourneinc.com>
2011-08-26 15:00:58 -0700

    Updated the Autoloader Unit tests

--- test/classes/Autoloader.Test.php
+++ test/classes/Autoloader.Test.php
@@ -2,6 +2,8 @@
 require_once 'base/Test.php';
 require_once 'classes/Autoloader.class.php';
 
+use \Cumula\Autoloader as Autoloader;
+
 /**
  * Unit Tests for the Cumula Autoloader
  * @package Cumula
@@ -18,8 +20,19 @@ class Test_Autoloader extends Test_BaseTest
 	{
 		parent::setUp();
 
-		\Cumula\Autoloader::setup();
+		Autoloader::setup();
 	} // end function setUP
+
+	/**
+	 * tearDown
+	 * @param void
+	 * @return void
+	 **/
+	public function tearDown() 
+	{
+		$autoloader = new Autoloader;
+		Autoloader::setInstance($autoloader);
+	} // end function tearDown
 
 	/**
 	 * Make sure the Autoloader was registered
@@ -30,8 +43,26 @@ class Test_Autoloader extends Test_BaseTest
 	 **/
 	public function testAutoloaderRegistered() 
 	{
-		\Cumula\Autoloader::setup();
+		Autoloader::setup();
 		$functions = spl_autoload_functions();
 		$this->assertTrue(in_array(array('Cumula\\Autoloader', 'load'), $functions));
 	} // end function testAutoloaderRegistered
+
+	/**
+	 * Test the absoluteClassName method
+	 * @param void
+	 * @return void
+	 * @group all
+	 * @covers Cumula\Autoloader::absoluteClassName
+	 **/
+	public function testAbsoluteClassName() 
+	{
+		// Populate the Autoloader
+		Autoloader::load('SomeMiserableClass');
+
+		$this->assertEquals('Cumula\\Router', Autoloader::absoluteClassName('Router'));
+
+		Autoloader::getInstance()->registerClass('MyAutoloaderClass\\Router', __FILE__);
+		$this->assertEquals('MyAutoloaderClass\\Router', Autoloader::absoluteClassName('Router'));
+	} // end function testAbsoluteClassName
 } // end class Test_Autoloader extends Test_BaseTest
