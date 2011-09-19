79ac49c569303304e5239e64a2ddf91280997aa8
Craig Gardner <craig@seabourneinc.com>
2011-09-19 16:39:28 -0700

    Added Unit Tests for the Cache component

---
+++ test/components/Cache.Test.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'base/Test.php';
+require_once 'components/Cache/Cache.component';
+
+/**
+ * Cache Component Tests
+ * @package Cumula
+ * @author Craig Gardner <craig@seabourneconsulting.com>
+ **/
+class Test_Cache extends Test_BaseTest 
+{
+	/**
+	 * setUp
+	 * @param void
+	 * @return void
+	 **/
+	public function setUp() 
+	{
+		parent::setUp();
+		defined('APPROOT') ||
+			define('APPROOT', vfsStream::url('app'));
+		defined('CONFIGROOT') ||
+			define('CONFIGROOT', vfsStream::url('app/config'));
+		\Cache\Cache::getInstance()->startup();
+	} // end function setUp
+
+	/**
+	 * Test the get method
+	 * @param void
+	 * @return void	
+	 * @group all
+	 **/
+	public function testCacheGet() 
+	{
+		$key = uniqid('key_');
+		$value = uniqid('value_');
+		$this->assertFalse(\Cache\Cache::get($key));
+
+		\Cache\Cache::set($key, $value);
+		$this->assertEquals(\Cache\Cache::get($key), $value);
+	} // end function testCacheGet
+
+	/**
+	 * Test the Cache class with a custom data store
+	 * @param void
+	 * @return void
+	 * @group all
+	 **/
+	public function testCustomDataStore() 
+	{
+		$schema = $this->getMockBuilder('Cumula\\CumulaSchema')->getMock();
+
+		$dataStore = $this->getMockBuilder('Cumula\\BaseDataStore')
+			->setConstructorArgs(array($schema))
+			->getMock();
+		$cache = \Cache\Cache::getInstance();
+		$cache->addDataStore('test', $dataStore);
+		$this->assertEquals($cache->dataStoreExists('test'), $dataStore);
+		$this->assertEquals($cache->getDataStore('test'), $dataStore);
+
+		$this->assertFalse($cache->addDataStore('cache', $dataStore), 'Make sure we cannot overwrite the default data store');
+
+	} // end function testCustomDataStore
+} // end class Test_Cache extends Test_BaseTest
