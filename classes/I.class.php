f253bcd211d0a4336bfffcad8bae6b9a015bce5e
Mike Reich <mike@seabourneconsulting.com>
2011-10-25 14:31:13 -0400

    Rearranged things

--- classes/I.class.php
+++ classes/I.class.php
@@ -2,6 +2,14 @@
 
 class DummyComponent {
 	public function __call($name, $args) {
+		return $this->_triggerError();
+	}
+	
+	public function __get($name) {
+		return $this->_triggerError();
+	}
+	
+	private function _triggerError() {
 		trigger_error('You called an instance which doesnt exist');
 	}
 }
